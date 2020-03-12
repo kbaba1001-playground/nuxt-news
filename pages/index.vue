6577d44c423dc55f2b4c7e25dc69f2479e827793
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-03-12 11:10:13 +0900

    country state

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="md-layout md-alignment-center" style="margin: 4em 0;">
     <!-- Top Navigation -->
     <md-toolbar class="fixed-toolbar" elevation="1">
-      <md-button class="md-icon-button">
+      <md-button @click="showLeftSidepanel = true" class="md-icon-button">
         <md-icon>menu</md-icon>
       </md-button>
       <nuxt-link class="md-primary md-title" to="/">
@@ -12,12 +12,32 @@
       <div class="md-toolbar-section-end">
         <md-button to="/login">Login</md-button>
         <md-button to="/register">Register</md-button>
-        <md-button class="md-accent" @click="showSidepanel = true">Categories</md-button>
+        <md-button class="md-accent" @click="showRightSidepanel = true">Categories</md-button>
       </div>
     </md-toolbar>
 
+    <!-- Personal News Feed -->
+    <md-drawer md-fixed :md-active.sync="showLeftSidepanel">
+      <md-toolbar md-elevation="1">
+        <span class="md-title">Personal Feed</span>
+      </md-toolbar>
+
+      <md-progress-bar v-if="loading" md-mode="indeterminate"></md-progress-bar>
+
+      <md-field>
+        <label for="country">Country</label>
+        <md-select @input="changeCountry" name="country" id="country">
+          <md-option value="us">United States</md-option>
+          <md-option value="ca">Canada</md-option>
+          <md-option value="de">Germany</md-option>
+          <md-option value="ru">Russia</md-option>
+          <md-option value="jp">Japan</md-option>
+        </md-select>
+      </md-field>
+    </md-drawer>
+
     <!-- News categories -->
-    <md-drawer class="md-right" md-fixed :md-active.sync="showSidepanel">
+    <md-drawer class="md-right" md-fixed :md-active.sync="showRightSidepanel">
       <md-toolbar :md-elevation="1">
         <span class="md-title">New Categories</span>
       </md-toolbar>
@@ -96,7 +116,8 @@
 <script>
 export default {
   data: () => ({
-    showSidepanel: false,
+    showLeftSidepanel: false,
+    showRightSidepanel: false,
     newsCategories: [
       { name: 'Top Headlines', path: '', icon: 'today' },
       { name: 'Technology', path: 'technology', icon: 'keyboard' },
@@ -108,7 +129,12 @@ export default {
     ]
   }),
   async fetch({store}) {
-    await store.dispatch('loadHeadlines', `/api/top-headlines?country=us&category=${store.state.category}`)
+    await store.dispatch('loadHeadlines', `/api/top-headlines?country=${store.state.country}&category=${store.state.category}`)
+  },
+  watch: {
+    async country() {
+      await this.$store.dispatch('loadHeadlines', `/api/top-headlines?country=${this.country}&category=${this.category}`)
+    }
   },
   computed: {
     headlines() {
@@ -119,12 +145,18 @@ export default {
     },
     loading() {
       return this.$store.getters.loading;
+    },
+    country() {
+      return this.$store.getters.country;
     }
   },
   methods: {
     async loadCategory(category) {
       this.$store.commit('setCategory', category)
-      await this.$store.dispatch('loadHeadlines', `/api/top-headlines?country=us&category=${this.category}`)
+      await this.$store.dispatch('loadHeadlines', `/api/top-headlines?country=${this.country}&category=${this.category}`)
+    },
+    changeCountry(country) {
+      this.$store.commit('setCountry', country);
     }
   }
 };
